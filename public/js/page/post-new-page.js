2782ff4546917440f972c743027d8f15c71040c0
Daniel Chung <danielvchung@hotmail.com>
2019-12-13 15:43:32 -0300

    modelando validacoes de dados

--- public/js/page/post-new-page.js
+++ public/js/page/post-new-page.js
@@ -1,4 +1,4 @@
-define(["require", "exports", "../http/post-http"], function (require, exports, post_http_1) {
+define(["require", "exports", "../http/post-http", "../components/form", "../components/validators/validators", "../components/validators/validator-manager"], function (require, exports, post_http_1, form_1, validators_1, validator_manager_1) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     class PostNewPage {
@@ -7,8 +7,6 @@ define(["require", "exports", "../http/post-http"], function (require, exports, 
             this.init();
         }
         init() {
-            this.postHttp.save({ title: 'titulo', body: 'conteudo' })
-                .then(obj => console.log(obj));
             document.querySelector('#my-form')
                 .addEventListener('submit', (event) => {
                 event.preventDefault();
@@ -16,7 +14,25 @@ define(["require", "exports", "../http/post-http"], function (require, exports, 
                 return false;
             });
         }
-        submit() { }
+        submit() {
+            if (!this.isValid()) {
+                return;
+            }
+            this.postHttp.save({
+                title: form_1.default.getValueFromField('#title'),
+                body: form_1.default.getValueFromField('#body')
+            }).then(obj => this.goToPostList());
+        }
+        goToPostList() {
+            window.location.href = '/post/post-list.html';
+        }
+        isValid() {
+            const validator = new validator_manager_1.default([
+                { selectorField: '#title', rules: [validators_1.default.required], messageInvalid: 'Título inválido.' },
+                { selectorField: '#body', rules: [validators_1.default.required], messageInvalid: 'Conteúdo inválido.' }
+            ]);
+            return validator.isValid();
+        }
     }
     exports.default = PostNewPage;
     new PostNewPage(new post_http_1.default());
--- src/page/post-new-page.ts
+++ src/page/post-new-page.ts
@@ -1,5 +1,8 @@
 import Page from './page';
 import PostHttp from '../http/post-http';
+import Form from '../components/form';
+import Validators from '../components/validators/validators';
+import ValidatorManager from '../components/validators/validator-manager';
 
 export default class PostNewPage implements Page {
 	constructor(private postHttp: PostHttp) {
@@ -7,8 +10,6 @@ export default class PostNewPage implements Page {
 	}
 
 	init(): void {
-		this.postHttp.save({title: 'titulo', body: 'conteudo'})
-			.then(obj => console.log(obj));
 		document.querySelector('#my-form')
 			.addEventListener('submit', (event: Event) => {
 				event.preventDefault();
@@ -17,9 +18,27 @@ export default class PostNewPage implements Page {
 			});
 	}
 
-	submit() {}
+	submit() {
+		if (!this.isValid()) {
+			return;
+		}
+		this.postHttp.save({
+			title: Form.getValueFromField('#title'),
+			body: Form.getValueFromField('#body')
+		}).then(obj => this.goToPostList());
+	}
 
-	// isValid(): boolean {}
+	goToPostList(): void {
+		window.location.href = '/post/post-list.html';
+	}
+
+	isValid(): boolean {
+		const validator = new ValidatorManager([
+			{selectorField: '#title', rules: [Validators.required], messageInvalid: 'Título inválido.'},
+			{selectorField: '#body', rules: [Validators.required], messageInvalid: 'Conteúdo inválido.'}
+		]);
+		return validator.isValid();
+	}
 }
 
 new PostNewPage(new PostHttp());
